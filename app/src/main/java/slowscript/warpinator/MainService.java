8ab8867bc52dba9f8450dc5da6fab73480476977
slowscript <slowscript@seznam.cz>
2020-10-14 20:31:54 +0200

    Make service run in foreground

--- app/src/main/java/slowscript/warpinator/MainService.java
+++ app/src/main/java/slowscript/warpinator/MainService.java
@@ -1,11 +1,22 @@
 package slowscript.warpinator;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import java.util.LinkedHashMap;
 import java.util.Timer;
@@ -15,6 +26,8 @@ public class MainService extends Service {
 
     static String TAG = "SERVICE";
     static int PORT = 42000;
+    static String CHANNEL_ID = "MainService";
+    static String ACTION_STOP = "StopSvc";
     static long pingTime = 10_000;
 
     public static Server server;
@@ -22,6 +35,7 @@ public class MainService extends Service {
     public TransfersActivity transfersView;
 
     public static MainService svc;
+    public NotificationManagerCompat notificationMgr;
     Timer pingTimer;
 
     @Nullable
@@ -34,6 +48,7 @@ public class MainService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
         svc = this;
+        notificationMgr = NotificationManagerCompat.from(this);
 
         Authenticator.getServerCertificate(); //Generate cert on start if doesn't exist
         Utils.getSaveDir().mkdirs();
@@ -49,6 +64,28 @@ public class MainService extends Service {
                 pingRemotes();
             }
         }, 0L, pingTime);
+
+        // Notification related stuff
+        createNotificationChannel();
+        Intent openIntent = new Intent(this, MainActivity.class);
+        openIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, openIntent, 0);
+
+        Intent stopIntent = new Intent(this, StopSvcReceiver.class);
+        stopIntent.setAction(ACTION_STOP);
+        PendingIntent stopPendingIntent =
+                PendingIntent.getBroadcast(this, 0, stopIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Warpinator service is running")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent)
+                .addAction(0, "Stop service", stopPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setShowWhen(false)
+                .setOngoing(true).build();
+
+        startForeground(2589, notification);
 
         return START_STICKY;
     }
@@ -92,5 +129,29 @@ public class MainService extends Service {
         MainActivity.current.updateRemoteList();
         //Remove
         remotes.remove(uuid);
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Service running";//getString(R.string.channel_name);
+            String description = "Persistent service notification";//getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_MIN;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public static class StopSvcReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (ACTION_STOP.equals(intent.getAction())) {
+                context.stopService(new Intent(context, MainService.class));
+                if (MainActivity.current != null)
+                    MainActivity.current.finishAffinity();
+            }
+        }
     }
 }
