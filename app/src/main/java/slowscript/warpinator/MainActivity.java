d9a9e740da0bfe22eb1b432532a953cb85476a1c
slowscript <slowscript@seznam.cz>
2021-08-28 21:16:46 +0200

    Fix some crashes and an ANR
    Fix jmDNS related crash by upgrading to newer version
    Fix crash when we got no uri to share
    Run stopMDNS on another thread to prevent ANR

--- app/src/main/java/slowscript/warpinator/MainActivity.java
+++ app/src/main/java/slowscript/warpinator/MainActivity.java
@@ -14,7 +14,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,8 +32,8 @@ import java.security.Security;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String TAG = "MAIN";
-    private final String helpUrl = "https://github.com/slowscript/warpinator-android/blob/master/connection-issues.md";
+    private static final String TAG = "MAIN";
+    private static final String helpUrl = "https://github.com/slowscript/warpinator-android/blob/master/connection-issues.md";
 
     RecyclerView recyclerView;
     RemotesAdapter adapter;
